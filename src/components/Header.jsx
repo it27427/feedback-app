24cd129d5b27ee7bf72b7ef849884df4f9a19a50
it27427 <istiak.tushar.it@gmail.com>
2024-03-27 22:33:20 +0600

    tailwind css added and header-component completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,26 @@
+import PropTypes from 'prop-types';
+
 import Logo from '../assets/images/logo.svg';
 
-const Header = () => {
+const Header = ({ text }) => {
   return (
-    <header>
+    <header className='bg-sky-900 py-3'>
       <div className='container'>
         <div className='flex items-center'>
-          <img src={Logo} alt='Brand-Logo' className='mr-2' />
-          <h1>Feedback UI</h1>
+          <img src={Logo} width={36} alt='Brand-Logo' className='mr-2' />
+          <h1 className='text-white font-bold uppercase'>{text}</h1>
         </div>
       </div>
     </header>
   );
+};
+
+Header.defaultProps = {
+  text: 'Feedback UI',
+};
+
+Header.propTypes = {
+  text: PropTypes.string,
 };
 
 export default Header;
