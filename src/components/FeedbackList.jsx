fd81b96a80af6dbdd2316f23b652f5d68d4d03f5
it27427 <istiak.tushar.it@gmail.com>
2024-03-29 17:56:05 +0600

    Events & Prop Drilling

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
-const FeedbackList = ({ feedback }) => {
+const FeedbackList = ({ feedback, handleDelete }) => {
   if (!feedback || feedback.length === 0) {
-    return <p className='text-indigo-100 font-semibold'>No Feedback Yet!</p>;
+    return (
+      <div className='p-4'>
+        <p className='text-indigo-100 font-semibold'>No Feedback Yet!</p>
+      </div>
+    );
   }
 
   return (
-    <div className='flex flex-col gap-8 pt-8'>
+    <div className='flex flex-col gap-8 pt-8 px-4'>
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
