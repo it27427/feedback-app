dd58d40b1f38d52580c1f8b7500b602200102dfc
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-04 11:43:25 +0600

    Update & Delete From JSON-Server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -39,11 +39,19 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // UPDATE FEEDBACK-ITEM
-  const updateFeedback = (id, updatedItem) => {
+  const updateFeedback = async (id, updatedItem) => {
+    const response = await fetch(`/api/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedItem),
+    });
+
+    const data = await response.json();
+
     setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...updatedItem } : item
-      )
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
@@ -56,8 +64,11 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // DELETE-FEEDBACK
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure want to delete?')) {
+      await fetch(`/api/feedback/${id}`, {
+        method: 'DELETE',
+      });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
